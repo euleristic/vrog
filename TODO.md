7e54434b086271b7c7fcff070e9badd1e0b2d1cd
Valter Ottenvang <valter.ottenvang@gmail.com>
2026-01-23 22:59:44 +0100

    Added docstrings, flag customization in Linker and Compiler, and fixed some typos/bugs

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO
 
-- Add docstrings
 - Add compiler factories for debug, release, and other profiles
 - Add automatic header dependency
 - Add build/source directory support
--- vrog.py
+++ vrog.py
@@ -3,7 +3,16 @@ import subprocess
 import dataclasses
 
 class BuildRule:
+    """A BuildRule builds a target from its dependencies."""
     def __init__(self, deps: list[str], task):
+        """Initializes a BuildRule.
+
+        deps
+          The list of dependencies to build the target.
+        task
+          The function which builds the target from the dependencies.
+          This function should have arguments (self: BuildRule, target: str)
+        """
         if type(deps) is not list:
             raise TypeError(f"deps {deps} should be a list")
         self.deps = deps
@@ -13,26 +22,54 @@ class BuildRule:
         self.task_impl = task
 
 
-    def task(self, target):
+    def task(self, target: str) -> None:
+        """Calls the task to build target."""
         self.task_impl(self, target)
 
 
 @dataclasses.dataclass
 class Compiler():
+    """Abstracts a compiler invocation.
+
+    compiler
+      The program to execute.
+    standard
+      Argument passed with self.standard_option, if not None.
+    optimization
+      Argument passed with self.standard_option, if not None.
+    warnings
+      Arguments passed with self.warning_option.
+    definitions
+      Arguments passed with self.definition_option.
+    extra_args
+      Arguments passed as they are.
+    """
     compiler: str="cc"
     standard: str=None
-    optimization: int=None
+    standard_option: str="-std="
+    optimization: str=None
+    optimization_option: str="-O"
     warnings: list[str]=dataclasses.field(default_factory=list)
+    warning_option: str="-W"
     definitions: list[str]=dataclasses.field(default_factory=list)
+    definition_option: str="-D"
     extra_args: list[str]=dataclasses.field(default_factory=list)
 
 
 class CompilerRule(BuildRule):
+    """A rule which compiles an object file from a source file."""
     def __init__(
         self,
         source: str,
         compiler: Compiler=Compiler()
     ):
+        """Initializes a CompilerRule.
+
+        source
+          The source file to compile.
+        compiler
+          The compiler to invoke.
+        """
         if type(source) is not str:
             raise TypeError(f"source {source} should be a string")
         if type(compiler) is not Compiler:
@@ -41,13 +78,17 @@ class CompilerRule(BuildRule):
         self.compiler = compiler
 
 
-    def task(self, target):
+    def task(self, target: str) -> None:
+        """Invokes the compiler to build target"""
         if type(target) is not str:
             raise TypeError(f"target {target} should be a string")
         cmd = [self.compiler.compiler, "-c"]
-        if self.compiler.standard: cmd.append(f"-std={self.compiler.standard}")
-        if self.compiler.optimization: cmd.append(f"-o{self.compiler.optimization}")
-        cmd += ["-W" + warning for warning in self.compiler.warnings]
+        if self.compiler.standard:
+            cmd.append(self.compiler.standard_option + self.compiler.standard)
+        if self.compiler.optimization:
+            cmd.append(self.optimization_option + self.compiler.optimization)
+        cmd += [warning_option + warning for warning in self.compiler.warnings]
+        cmd += [definition_option + definition for definition in self.compiler.definitions]
         cmd += self.compiler.extra_args
         cmd += self.deps
         cmd += ["-o", target]
@@ -56,18 +97,39 @@ class CompilerRule(BuildRule):
 
 @dataclasses.dataclass
 class Linker():
+    """Abstracts a linker invocation.
+
+    compiler
+      The program to execute.
+    libraries
+      Arguments passed with self.library_option.
+    linker_args
+      Arguments passed with self.linker_arg_option.
+    extra_args
+      Arguments passed as they are.
+    """
     linker: str="cc"
     libraries: list[str]=dataclasses.field(default_factory=list)
+    library_option: str="-l"
     linker_args: list[str]=dataclasses.field(default_factory=list)
+    linker_arg_option: str="-Wl,"
     extra_args: list[str]=dataclasses.field(default_factory=list)
 
 
 class LinkerRule(BuildRule):
+    """A rule which links a target from object files."""
     def __init__(
         self,
         objects: list[str],
         linker: Linker=Linker()
     ):
+        """Initializes a LinkerRule.
+
+        objects
+          The object file to link.
+        compiler
+          The linker to invoke.
+        """
         if type(objects) is not list:
             raise TypeError(f"objects {objects} should be a list")
         if type(linker) is not Linker:
@@ -76,12 +138,13 @@ class LinkerRule(BuildRule):
         self.linker = linker
 
 
-    def task(self, target):
+    def task(self, target: str) -> None:
+        """Invokes the linker to build target."""
         if type(target) is not str:
             raise TypeError(f"target {target} should be a string")
         cmd = [self.linker.linker]
-        cmd += ["-l" + lib for lib in self.linker.libraries]
-        cmd += ["-Wl," + lib for lib in self.linker.linker_args]
+        cmd += [self.linker.library_option + lib for lib in self.linker.libraries]
+        cmd += [self.linker.linker_arg_option + link for link in self.linker.linker_args]
         cmd += self.linker.extra_args
         cmd += self.deps
         cmd += ["-o", target]
@@ -89,7 +152,13 @@ class LinkerRule(BuildRule):
 
 
 class CleanRule(BuildRule):
-    def __init__(self, targets):
+    """A rule for cleaning targets."""
+    def __init__(self, targets: list[str]):
+        """Initializes a CleanRule.
+
+        targets
+          Targets to remove on clean.
+        """
         self.deps = []
         if type(targets) is not list:
             raise TypeError(f"targets {targets} should be a list")
@@ -97,6 +166,7 @@ class CleanRule(BuildRule):
 
 
     def task(self, target):
+        """Removes self.targets. target argument is ignored."""
         for file in self.targets:
             if os.path.exists(file):
                 print(f"rm {file}")
@@ -104,11 +174,20 @@ class CleanRule(BuildRule):
 
 
 class BuildSystem:
+    """A system of rules for how to build targets."""
     def __init__(self):
+        """Initializes a BuildSystem."""
         self.rules = {}
 
 
     def add_rule(self, target: str, rule: BuildRule) -> None:
+        """Adds a rule to the BuildSystem.
+
+        target
+          The target of the rule.
+        rule
+          The BuildRule providing the target's dependencies and the task which builds it.
+        """
         if type(target) is not str:
             raise TypeError(f"target {target} should be a string")
         if not isinstance(rule, BuildRule):
@@ -122,18 +201,35 @@ class BuildSystem:
         sources: list[str],
         compiler: Compiler=Compiler(),
         linker: Linker=Linker()
-    ):
+    ) -> None:
+        """Adds a LinkerRule and CompilerRules to build a target from source files.
+
+        target
+          The target of the LinkerRule.
+        sources
+          The source files which build target.
+        compiler
+          The Compiler of the CompilerRules.
+        linker
+          The Linker of the LinkerRules.
+        """
         objects = [f"{source}.o" for source in sources]
         self.add_rule(target, LinkerRule(objects, linker))
         for obj, source in zip(objects, sources):
             self.add_rule(obj, CompilerRule(source, compiler))
 
 
-    def add_clean(self):
-        self.add_rule("clean", CleanRule(list(self.rules.keys())))
+    def add_clean(self, target: str="clean") -> None:
+        """Adds a CleanRules which cleans all targets in the BuildSystem.
+
+        target
+          The target to "build" which will invoke the rule.
+        """
+        self.add_rule(target, CleanRule(list(self.rules.keys())))
         
 
-    def build(self, target: str):
+    def build(self, target: str) -> None:
+        """Builds target, building dependencies as necessary to keep target up to date"""
         if type(target) is not str:
             raise TypeError(f"target {target} should be a string")
 
@@ -159,29 +255,31 @@ class BuildSystem:
                 return
 
 
-    def circular(self, target: str, dependents: set[str]=set()) -> bool:
+    def circular(self, target: str, _dependents: set[str]=set()) -> bool:
+        """Checks if there is a dependency circularity for target"""
         if type(target) is not str:
             raise TypeError(f"target {target} should be a string")
 
-        if type(dependents) is not set:
-            raise TypeError(f"dependendts {dependents} should be a set")
+        if type(_dependents) is not set:
+            raise TypeError(f"_dependendts {_dependents} should be a set")
 
-        if target in dependents:
+        if target in _dependents:
             return True
 
         if target not in self.rules:
             return False
 
         for dep in self.rules[target].deps:
-            if self.circular(dep, dependents.union({target})):
+            if self.circular(dep, _dependents.union({target})):
                 return True
 
         return False
 
 
 def run_cmd(cmd: str) -> subprocess.CompletedProcess:
-        if type(cmd) is not str:
-            raise TypeError(f"cmd {cmd} should be a string")
-        return subprocess.run(["sh", "-c", cmd], check=True)
+    """Runs cmd through the shell"""
+    if type(cmd) is not str:
+        raise TypeError(f"cmd {cmd} should be a string")
+    return subprocess.run(["sh", "-c", cmd], check=True)
 
 
